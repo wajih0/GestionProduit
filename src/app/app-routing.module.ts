a6d09f2e949bc31f71f2c0524f70ea13ec203acf
unknown <wajihbenhmida5@gmail.com>
2023-07-07 20:55:12 +0100

    ajout routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProduitComponent } from './produit/produit.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'produits', component: ProduitComponent }
+  { path: 'produit', component: ProduitComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashbord',pathMatch: 'full'}
+
 ];
 
 @NgModule({
